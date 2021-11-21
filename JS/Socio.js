b5f834813f9cfcf097a332d4e29e3d4f61d8649a
SergioQuiroz <sergio.quiroz@unah.hn>
2021-11-20 21:26:37 -0600

    nueva solucion

--- JS/Socio.js
+++ JS/Socio.js
@@ -1,8 +1,8 @@
-var UrlGetSocios ='http://localhost:90/G8_20/Controller/socios.php?op=Getsocios';
-var UrlInsertarSocios ='http://localhost:90/G8_20/Controller/socios.php?op=Insert_socios';
-var UrlGetUno ='http://localhost:90/G8_20/Controller/socios.php?op=GetUno';
-var UrlActualizarSocios ='http://localhost:90/G8_20/Controller/socios.php?op=Update_socios';
-var UrlEliminarSocios = 'http://localhost:90/G8_20/Controller/socios.php?op=Delete_socios';
+var UrlGetSocios ='http://localhost:90/G8_20/Socios_negocios/Controller/socios.php?op=Getsocios';
+var UrlInsertarSocios ='http://localhost:90/G8_20/Socios_negocios/Controller/socios.php?op=Insert_socios';
+var UrlGetUno ='http://localhost:90/G8_20/Socios_negocios/Controller/socios.php?op=GetUno';
+var UrlActualizarSocios ='http://localhost:90/G8_20/Socios_negocios/Controller/socios.php?op=Update_socios';
+var UrlEliminarSocios = 'http://localhost:90/G8_20/Socios_negocios/Controller/socios.php?op=Delete_socios';
 
 $(document).ready(function(){
     CargarSocios();
